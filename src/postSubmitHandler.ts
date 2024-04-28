2edc965f0f6f82522f02076567ef6507f2dd9f10
fsvreddit <john@mclusky.co.uk>
2024-04-28 13:46:00 +0100

    Move console.log call

--- src/postSubmitHandler.ts
+++ src/postSubmitHandler.ts
@@ -4,7 +4,7 @@ import {POST_LIST} from "./constants.js";
 
 export async function handlePostSubmitEvent (event: PostSubmit, context: TriggerContext) {
     if (event.post) {
-        console.log(`Added post ${event.post.id} to list for future checking. CreatedAt: ${event.post.createdAt}`);
         await context.redis.zAdd(POST_LIST, {member: event.post.id, score: event.post.createdAt});
+        console.log(`Added post ${event.post.id} to list for future checking. CreatedAt: ${event.post.createdAt}`);
     }
 }
